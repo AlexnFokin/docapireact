9c59feaee5f15faba1c81e0a86dfc73f9f48550b
Alexnfokin <ax9642824@gmail.com>
2025-03-28 17:02:48 +0300

    added eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,41 +1,41 @@
-  import { defineConfig } from "eslint/config";
-  import globals from "globals";
-  import js from "@eslint/js";
-  import tseslint from "typescript-eslint";
-  import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
 
-  export default defineConfig([
-      { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-      { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { 
+export default defineConfig([
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { 
         globals: globals.browser
-      } 
+    } 
     },
-      { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-      tseslint.configs.recommended,
-      pluginReact.configs.flat.recommended,
-      {
-          rules: {
-              "react/react-in-jsx-scope": "off",
-              "indent": ["error", 4, {
-                  "SwitchCase": 1, 
-                  "VariableDeclarator": 1, 
-              }],
-              "operator-linebreak": ["error", "before", { "overrides": { "?": "ignore", ":": "ignore" }}],
-              "no-unused-vars": "warn",
-              "react/require-default-props": "off",
-              "react/jsx-props-no-spreading": "warn",
-              "react/function-component-definition": "off",
-              "no-shadow": "off",
-              "import/extentions": "off",
-              "import/no-extraneous-dependencies": "off",
-              "no-underscore-dangle": "off"
-          },
-          settings: {
-              react: {
-                  version: "detect",
-              },
-          },
-          ignores: ["build/**/*"],
-      },
-  ]);
+    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "indent": ["error", 4, {
+                "SwitchCase": 1, 
+                "VariableDeclarator": 1, 
+            }],
+            "operator-linebreak": ["error", "before", { "overrides": { "?": "ignore", ":": "ignore" }}],
+            "no-unused-vars": "warn",
+            "react/require-default-props": "off",
+            "react/jsx-props-no-spreading": "warn",
+            "react/function-component-definition": "off",
+            "no-shadow": "off",
+            "import/extentions": "off",
+            "import/no-extraneous-dependencies": "off",
+            "no-underscore-dangle": "off"
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+        ignores: ["build/**/*"],
+    },
+]);
